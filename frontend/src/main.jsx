952f47534c96557fce2a9a70ebceee27d919b149
MichielUijting <169062718+MichielUijting@users.noreply.github.com>
2026-03-01 12:25:06 +0100

    Auto update via BAT: Rezzerv-v01.04.55.zip

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import "./ui/tokens.css";
 import "./ui/base.css";
 import "./ui/components/button.css";
 import "./ui/components/card.css";
 import "./ui/components/header.css";
 import "./ui/components/table.css";
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
