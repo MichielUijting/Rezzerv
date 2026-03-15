90cf948400694c568ddad9abc67677dc70382a1a
MichielUijting <169062718+MichielUijting@users.noreply.github.com>
2026-03-15 14:19:10 +0100

    Auto update via BAT: Rezzerv-v01.08.18.zip

--- backend/app/main.py
+++ backend/app/main.py
@@ -45,10 +45,10 @@ users = {
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "http://localhost:5180",
+        "http://127.0.0.1:5180",
     ],
     allow_credentials=True,
     allow_methods=["*"],
