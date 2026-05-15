121a26dc1e00e04d47553deaa7c0b971d41c03cc
MichielUijting <169062718+MichielUijting@users.noreply.github.com>
2026-05-15 13:28:42 +0200

    Add parser augmentation contracts

---
+++ backend/receipt_ingestion/parser_augmentation/contracts.py
@@ -0,0 +1,15 @@
+AUGMENTATION_MODE = 'shadow'
+MIN_CONFIDENCE = 0.85
+
+FORBIDDEN_PRODUCT_TOKENS = {
+    'totaal',
+    'total',
+    'subtotaal',
+    'subtotal',
+    'betaling',
+    'pin',
+    'visa',
+    'mastercard',
+    'korting',
+    'discount',
+}
