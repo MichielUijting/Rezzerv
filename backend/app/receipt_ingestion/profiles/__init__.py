233c2d59274a4c8b5b8563fcc5a44caa3f44bce3
MichielUijting <169062718+MichielUijting@users.noreply.github.com>
2026-05-25 19:07:12 +0200

    R9-31A add profiles package

---
+++ backend/app/receipt_ingestion/profiles/__init__.py
@@ -0,0 +1,10 @@
+from .ah import AhReceiptProfile
+from .base import ProfileDetection, ProfileDiagnostics, ProfileLineClassification, READ_ONLY_PROFILE_GUARDRAILS
+
+__all__ = [
+    'AhReceiptProfile',
+    'ProfileDetection',
+    'ProfileDiagnostics',
+    'ProfileLineClassification',
+    'READ_ONLY_PROFILE_GUARDRAILS',
+]
