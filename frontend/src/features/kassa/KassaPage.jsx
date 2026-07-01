5cb8bcc199cbc1733a1d318bf2ee4099c1cf1e11
MichielUijting <169062718+MichielUijting@users.noreply.github.com>
2026-07-01 10:50:54 +0200

    fix(kassa): remove UI parse diagnostics from user flow

--- frontend/src/features/kassa/KassaPage.jsx
+++ frontend/src/features/kassa/KassaPage.jsx
@@ -1,14 +1,1 @@
-import { useLayoutEffect } from 'react'
-import ReceiptsKassaPage from '../receipts/KassaPage.jsx'
-import KassaParseQualityDiagnostics, { installKassaReceiptDiagnosticsFetchProbe } from './KassaParseQualityDiagnostics.jsx'
-
-export default function KassaPage() {
-  useLayoutEffect(() => installKassaReceiptDiagnosticsFetchProbe(), [])
-
-  return (
-    <>
-      <ReceiptsKassaPage />
-      <KassaParseQualityDiagnostics />
-    </>
-  )
-}
+export { default } from '../receipts/KassaPage.jsx'
